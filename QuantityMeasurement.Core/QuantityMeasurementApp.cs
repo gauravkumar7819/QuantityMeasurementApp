12e9c8f38db559749dd37c5c595799ee8bc0e7b8
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2026-02-21 12:56:03 +0530

    [Gaurav Kumar].Add - Implemented Inches measurement equality and tests

--- QuantityMeasurement.Core/QuantityMeasurementApp.cs
+++ QuantityMeasurement.Core/QuantityMeasurementApp.cs
@@ -4,13 +4,20 @@ namespace QuantityMeasurement.Core
 {
     public class QuantityMeasurementApp
     {
-        // UC1: Feet equality check
+        // UC1: Feet equality
         public static bool AreFeetEqual(double firstFeet, double secondFeet)
         {
             var feet1 = new Feet(firstFeet);
             var feet2 = new Feet(secondFeet);
-
             return feet1.Equals(feet2);
+        }
+
+        // UC2: Inches equality
+        public static bool AreInchesEqual(double firstInches, double secondInches)
+        {
+            var inch1 = new Inches(firstInches);
+            var inch2 = new Inches(secondInches);
+            return inch1.Equals(inch2);
         }
     }
 }
